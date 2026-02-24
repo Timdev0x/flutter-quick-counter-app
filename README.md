b221d28a094be9f5bc8f979b91cc61f79e31a331
Timdev0x <tim.dev2019@gmail.com>
2026-02-24 23:20:03 +0300

    Initial commit: Quick Flutter bootstrap app

---
+++ README.md
@@ -0,0 +1,15 @@
+# Quick Flutter App
+
+A minimal Flutter counter app scaffold.
+
+## Run
+
+Open a terminal and run:
+
+```powershell
+cd "C:\Users\Hp\Desktop\Dev\mobile-aps\quick_flutter_app"
+flutter pub get
+flutter run
+```
+
+You can run on a specific device with `flutter run -d <device-id>`.
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quick Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const CounterPage(),
+    );
+  }
+}
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key});
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() => _counter++);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Counter')),
+      body: Center(
+        child: Text(
+          'You clicked $_counter times',
+          style: const TextStyle(fontSize: 20),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _increment,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
